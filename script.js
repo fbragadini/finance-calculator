9a0846a9e71fcad237d3cdd07cb8da3a880f977e
fbragadini <fbragadini@gmail.com>
2022-09-15 09:28:31 -0300

    actualizacion

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// VARIABLES PLAZO FIJO
+
 let nombre, monto, plazo, resultado;
 let calcularInteresCompuesto = false;
 const botonCalc = document.getElementById("botonCalcular");
@@ -5,6 +7,11 @@ const botonCalcIntComp = document.getElementById("calcIntComp");
 const historialPrestamos = []
 const tasa = 69.5;
 const coeficiente = parseFloat(tasa/12);
+
+
+
+
+//CLASES
 
 class Prestamo {
     constructor(nombre, monto, plazo, resultado) {        
@@ -15,6 +22,10 @@ class Prestamo {
         this.tasaInteres = "69.5%";
     }
 }
+
+
+
+//FUNCIONES PARA CALCULADORA PLAZO FIJO
 
 function calculadoraPF() {
     if (calcularInteresCompuesto) {
@@ -53,84 +64,5 @@ function calcIntComp() {
     }
 }
 
-/* TRACKER */
-const htmlTrackerTotal = document.getElementById("trackerTotal");
-let trackerResult = 0;
-let trackerEntries = [];
-let counter =  1;
-const trackerTable = document.getElementById("trackertable");
-let dollarUSLocale = Intl.NumberFormat('en-US');
-
-class Entry {
-    constructor (date, description, type, amount) {
-        this.id = counter;
-        this.date = date;
-        this.description = description;
-        this.type = type;
-        this.amount = amount;
-    }
-}
-
-function newEntry() {
-    const trackerEntry = document.getElementById("trackerForm")
-    const newEntry = new Entry(trackerEntry.entryDate.value, trackerEntry.entryDescription.value, trackerEntry.entryType.value, parseInt(trackerEntry.entryAmount.value));
-    trackerEntries.push(newEntry);
-    counter++;
-    console.log(trackerEntries);
-    updateTracker();
-    getResult();
-    saveEntries();
-}
-
-function updateTracker() {
-    trackerTable.innerHTML = ""
-    trackerEntries.forEach((entry) => {    
-    const entryHTML = document.createElement('tr');
-    entryHTML.innerHTML = `
-        <th scope="row">${entry.date}</th> 
-        <td>${entry.description}</td> 
-        <td>${entry.type}</td> 
-        <td>${entry.amount}</td>
-        <td>
-        <button class="deleteBtn" onclick="deleteEntry(event)" id="deleteBtn_${entry.id}">&#10060;</button>
-        </td>
-    `;    
-    trackerTable.appendChild(entryHTML);
-    })
-};
-
-function getResult() {
-    trackerResult = 0;
-    for (let i = 0; i < trackerEntries.length; i++) {
-        if (trackerEntries[i].type == "income") {
-                trackerResult += trackerEntries[i].amount;
-            } else {
-                trackerResult -= trackerEntries[i].amount;
-            }
-        }
-    console.log(trackerResult);
-    htmlTrackerTotal.innerText = '$' + dollarUSLocale.format(trackerResult);
-}
-
-function deleteEntry(event) {
-    let btn = event.target;
-    let id = btn.id.split("_")[1];
-    trackerEntries = trackerEntries.filter((entry) => entry.id != id);
-    updateTracker();
-    getResult();
-    saveEntries();
-}
-
-function saveEntries() {
-    localStorage.setItem("Entradas", JSON.stringify(trackerEntries));
-}
-
-function loadEntries () {
-    trackerEntries = JSON.parse(localStorage.getItem("Entradas"));
-    updateTracker();
-    getResult();
-}
-
-//botonCalc.addEventListener("click", calculadoraPF);
-//botonCalcIntComp.addEventListener("change", calcIntComp);
-loadEntries();
+botonCalc.addEventListener("click", calculadoraPF);
+botonCalcIntComp.addEventListener("change", calcIntComp);
--- trackerFinanzas.js
+++ trackerFinanzas.js
@@ -1,7 +1,16 @@
-const trackerEntries = [];
+//VARIABLES TRACKER GASTOS 
+const htmlTrackerTotal = document.getElementById("trackerTotal");
+let trackerResult = 0;
+let trackerEntries = [];
+let counter =  1;
+const trackerTable = document.getElementById("trackertable");
+let dollarUSLocale = Intl.NumberFormat('en-US');
+
+//CLASES
 
 class Entry {
     constructor (date, description, type, amount) {
+        this.id = counter;
         this.date = date;
         this.description = description;
         this.type = type;
@@ -9,23 +18,68 @@ class Entry {
     }
 }
 
+//FUNCIONES PARA EL TRACKER DE FINANZAS
+
 function newEntry() {
-    const trackerEntry = document.getElementById("trackerEntry");
-    const newEntry = new Entry(trackerEntry.entryDate.value, trackerEntry.entryDescription.value, trackerEntry.entryType.value, trackerEntry.entryAmount.value);
+    const trackerEntry = document.getElementById("trackerForm")
+    const newEntry = new Entry(trackerEntry.entryDate.value, trackerEntry.entryDescription.value, trackerEntry.entryType.value, parseInt(trackerEntry.entryAmount.value));
     trackerEntries.push(newEntry);
+    counter++;
     console.log(trackerEntries);
+    updateTracker();
+    getResult();
+    saveEntries();
 }
 
 function updateTracker() {
-    trackerEntries.forEach((entry) => { 
-    const table = document.getElementById("trackertable");
-    table.innerHTML = `
-    <tr>
-    <th scope="row"> ${entry.date} </th> 
-    <td>${entry.description}</td> 
-    <td>${entry.type}</td> 
-    <td>${entry.amount}</td>
-    </tr>`;
-    trackerEntry.appendChild(table);
+    trackerTable.innerHTML = ""
+    trackerEntries.forEach((entry) => {    
+    const entryHTML = document.createElement('tr');
+    entryHTML.innerHTML = `
+        <th scope="row">${entry.date}</th> 
+        <td>${entry.description}</td> 
+        <td>${entry.type}</td> 
+        <td>${entry.amount}</td>
+        <td>
+        <button class="deleteBtn" onclick="deleteEntry(event)" id="deleteBtn_${entry.id}">
+            &#10060;        
+        </button>
+        </td>
+    `;    
+    trackerTable.appendChild(entryHTML);
     })
 };
+
+function getResult() {
+    trackerResult = 0;
+    for (let i = 0; i < trackerEntries.length; i++) {
+        if (trackerEntries[i].type == "Ingreso") {
+                trackerResult += trackerEntries[i].amount;
+            } else {
+                trackerResult -= trackerEntries[i].amount;
+            }
+        }
+    console.log(trackerResult);
+    htmlTrackerTotal.innerText = '$' + dollarUSLocale.format(trackerResult);
+}
+
+function deleteEntry(event) {
+    let btn = event.target;
+    let id = btn.id.split("_")[1];
+    trackerEntries = trackerEntries.filter((entry) => entry.id != id);
+    updateTracker();
+    getResult();
+    saveEntries();
+}
+
+function saveEntries() {
+    localStorage.setItem("Entradas", JSON.stringify(trackerEntries));
+}
+
+function loadEntries() {
+    trackerEntries = JSON.parse(localStorage.getItem("Entradas"));
+    updateTracker();
+    getResult();
+}
+
+loadEntries();
